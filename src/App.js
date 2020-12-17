6f5e324e250ed8485407e4dd7551c5765cd5f1c8
triet <triettruong@itrvn.com>
2020-12-17 09:33:41 +0700

    first data

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-import Main from './Pages/main'
-import React from 'react';
+import React, { Component } from 'react';
+import Main from './Pages/Main';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      <Main/>
-      </header>
-    </div>
-  );
+import 'antd/dist/antd.css';
+import './Styles/style.scss';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Main />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Pages/main.js
+++ src/Pages/main.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-const SignIn = ()=><div><span>Sign In</span></div>
+import Amplify from '@aws-amplify/core';
+import NotFound from './NotFound';
+import Full from './Full';
+import PrivateRoute from '../Components/PrivateRoute';
+import SignIn from './SignIn';
+import ForgotPassword from './ForgotPassword';
+import auth from '../Helpers/auth';
+import awsmobile from '../../aws-exports';
 
+Amplify.configure(awsmobile);
 class Main extends React.Component {
   render() {
+    const { isSuccess } = this.props;
+    const authenticated = isSuccess || auth.isSuccess();
     return (
       <main>
-      <Router>
-      <Switch>
-        <Route path="/sign-in" name="Sign In" component={SignIn} />
-        {/* <PrivateRoute path="/" name="full" component={Full} authenticated={!!authenticated} /> */}
-      </Switch>
-      </Router>
+        <Router>
+          <Switch>
+            <Route path="/sign-in" name="Sign In" component={SignIn} />
+            <Route path="/forgot-password" name="Forgot password" component={ForgotPassword} />
+            <PrivateRoute path="/" name="full" component={Full} authenticated={!!authenticated} />
+            <Route path="*" name="notFound" component={NotFound} />
+          </Switch>
+        </Router>
       </main>
     );
   }
@@ -26,11 +39,10 @@ Main.defaultProps = {
 Main.propTypes = {
   isSuccess: PropTypes.bool,
 };
-export default Main;
 
-// <Router>
-// <Switch>
-//   <Route path="/sign-in" name="Sign In" component={SignIn} />
-//   {/* <PrivateRoute path="/" name="full" component={Full} authenticated={!!authenticated} /> */}
-// </Switch>
-// </Router>
+function mapStateToProps(state) {
+  return {
+    isSuccess: state.login.isSuccess,
+  };
+}
+export default connect(mapStateToProps)(Main);
