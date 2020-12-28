bcf4726637188192c3504aaee3047754142251dc
triet <triettruong@itrvn.com>
2020-12-28 18:57:07 +0700

    doing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Main from './Pages/main'
-import React from 'react';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      <Main/>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        <Main/>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
