fd869abef581f521d4f5e3d1494114fa7d900d1c
triet <triettruong@itrvn.com>
2020-12-30 18:22:41 +0700

    fixing route

--- src/Pages/main.js
+++ src/Pages/main.js
@@ -2,29 +2,41 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-const SignIn = ()=><div><span>Sign In</span></div>
+const SignIn = () => <div><span>Sign In</span></div>;
 
 class Main extends React.Component {
   render() {
     return (
-      <main>
-      <Router>
-      <Switch>
-        <Route path="/sign-in" name="Sign In" component={SignIn} />
-        {/* <PrivateRoute path="/" name="full" component={Full} authenticated={!!authenticated} /> */}
-      </Switch>
-      </Router>
-      </main>
+      <>
+        <div>
+          <span>
+            ABC
+          </span>
+        </div>
+        <Router>
+          <Switch>
+            <Route path="/sign-in" name="Sign In" component={SignIn} />
+            <div>
+              <span>
+                ABC123
+              </span>
+            </div>
+          </Switch>
+        </Router>
+        <div>
+          <span>
+            ABC
+          </span>
+        </div>
+      </>
     );
   }
 }
 
 Main.defaultProps = {
-  isSuccess: false,
 };
 
 Main.propTypes = {
-  isSuccess: PropTypes.bool,
 };
 export default Main;
 
