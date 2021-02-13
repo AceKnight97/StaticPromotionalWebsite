8e488883ed87af5dd908e2822975e9d2931cbf0e
triet <triettruong@itrvn.com>
2021-02-13 11:57:45 +0700

    update cv

---
+++ src/Pages/CVs/Layout/cvDetails.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const CVDetails = () => (
+  <div className='my-cv-2-right'>
+    <div className='wh100'>
+      <span>asdasdsd123</span>
+    </div>
+  </div>
+);
+
+CVDetails.defaultProps = {
+};
+CVDetails.propTypes = {
+};
+
+export default CVDetails;
--- src/Pages/CVs/myCV.js
+++ src/Pages/CVs/myCV.js
@@ -1,169 +1,18 @@
 import React from 'react';
-// import classnames from 'classnames';
-import {
-  MailOutlined, PhoneOutlined, GithubOutlined, EnvironmentOutlined,
-} from '@ant-design/icons';
-import _ from 'lodash';
-import { Button } from 'antd';
-import avatarIc from '../../Images/Pages/CVs/myAvatar.jpg';
-import InfoRow from './Layout/infoRow';
+import CVInformation from './Layout/cvInformation';
+import CVDetails from './Layout/cvDetails';
 
-const MyCV2 = () => {
-  const showHeader = () => (
-    <div className='my-cv-2-header'>
-      <img src={avatarIc} className='my-cv-2-avatar' alt='Avatar img' />
+const MyCV2 = () => (
+  <div className='my-cv-wrapper'>
+    <div className='my-cv-2-main'>
 
-      <div className='my-cv-2-names'>
-        <div className='my-cv-2-lastname'>
-          <span>Truong Thanh</span>
-        </div>
-        <div className='my-cv-2-firstname'>
-          <span>Triet</span>
-        </div>
-        <div className='my-cv-2-role'>
-          <span>Web-App developer</span>
-        </div>
-      </div>
+      <CVInformation />
+
+      <CVDetails />
+
     </div>
-  );
-
-  const showGeneralInfo = () => {
-    const arr = [
-      {
-        icon: <MailOutlined />,
-        title: 'tttriet1997@gmail.com',
-      },
-      {
-        icon: <PhoneOutlined />,
-        title: '(+84) 819 541 897',
-      },
-      {
-        icon: <GithubOutlined />,
-        title: 'https://aceknight97.github.io/greeting',
-        type: 'LINK',
-      },
-      {
-        icon: <EnvironmentOutlined />,
-        title: 'District 7, Hochiminh city',
-      },
-    ];
-    return (
-      _.map(arr, (x, i) => (
-        <InfoRow
-          className={i === 0 ? 'mt24' : 'mt16'}
-          key={i}
-          icon={x.icon}
-          title={x.title}
-          type={x?.type}
-        />
-      ))
-    );
-  };
-
-  const showIndustryKnowledge = () => {
-    const arr = [
-      'Web-App Develop',
-      'Technical Research',
-      'Team work',
-      'Project progressing',
-    ];
-    return (
-      <div className='my-cv-2-body'>
-        <div className='my-cv-2-body-title'>
-          <span>Industry Knowledge</span>
-        </div>
-
-        {_.map(arr, (x, i) => (
-          <div key={i} className='my-cv-2-body-content'>
-            <span className='a-dot'>●</span>
-            <span>{x}</span>
-          </div>
-        ))}
-      </div>
-    );
-  };
-
-  const showLanguages = () => {
-    const arr = [
-      'Vietnamese',
-      'English',
-    ];
-    return (
-      <div className='my-cv-2-body'>
-        <div className='my-cv-2-body-title'>
-          <span>Languages</span>
-        </div>
-
-        {_.map(arr, (x, i) => (
-          <div key={i} className='my-cv-2-body-content'>
-            <span>{x}</span>
-          </div>
-        ))}
-      </div>
-    );
-  };
-
-  const showSocial = () => {
-    const arr = [
-      'https://www.facebook.com/profile.php?id=100015087697713',
-      'https://www.linkedin.com/in/tri%E1%BA%BFt-tr%C6%B0%C6%A1ng-thanh-89a92b161/',
-    ];
-    return (
-      <div className='my-cv-2-body'>
-        <div className='my-cv-2-body-title'>
-          <span>Social</span>
-        </div>
-
-        {_.map(arr, (x, i) => (
-          <Button
-            key={i}
-            onClick={() => window.open(x, '')}
-            type='link'
-            ghost
-            className='my-cv-2-body-link'
-          >
-            {x}
-          </Button>
-        ))}
-      </div>
-    );
-  };
-
-  const showHobbies = () => {
-    const a = '';
-    return (
-      <div className='my-cv-2-body'>
-        <div className='my-cv-2-body-title'>
-          <span>Hobbies</span>
-        </div>
-      </div>
-    );
-  };
-
-  const showLeft = () => (
-    <div className='my-cv-2-left'>
-      {showHeader()}
-      {showGeneralInfo()}
-      {showIndustryKnowledge()}
-      {showLanguages()}
-      {showSocial()}
-      {showHobbies()}
-    </div>
-  );
-
-  return (
-    <div className='my-cv-wrapper'>
-      <div className='my-cv-2-main'>
-
-        {showLeft()}
-
-        <div className='my-cv-2-right'>
-          <span>asdasdsd</span>
-        </div>
-      </div>
-    </div>
-  );
-};
+  </div>
+);
 
 MyCV2.defaultProps = {
 };
