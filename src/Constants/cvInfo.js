7059dd666bd0115574a9becda1783dc0b015a6ec
triet <triettruong@itrvn.com>
2021-02-15 16:33:38 +0700

    fix

---
+++ src/Constants/cvInfo.js
@@ -0,0 +1,14 @@
+export const SOCICAL_DATA = [
+  'https://www.facebook.com/profile.php?id=100015087697713',
+  'https://www.linkedin.com/in/tri%E1%BA%BFt-tr%C6%B0%C6%A1ng-thanh-89a92b161/',
+];
+export const INDUSTRY_KNOWLEDGE_DATA = [
+  'Web-App Develop',
+  'Technical Research',
+  'Team work',
+  'Project progressing',
+];
+export const LANGUAGE_DATA = [
+  'Vietnamese',
+  'English',
+];
--- src/Constants/index.js
+++ src/Constants/index.js
@@ -1,14 +1,13 @@
-export const SOCICAL_DATA = [
-  'https://www.facebook.com/profile.php?id=100015087697713',
-  'https://www.linkedin.com/in/tri%E1%BA%BFt-tr%C6%B0%C6%A1ng-thanh-89a92b161/',
-];
-export const INDUSTRY_KNOWLEDGE_DATA = [
-  'Web-App Develop',
-  'Technical Research',
-  'Team work',
-  'Project progressing',
-];
-export const LANGUAGE_DATA = [
-  'Vietnamese',
-  'English',
-];
+const AppFlowActions = {
+  LOGIN_REQUEST: 'LOGIN_REQUEST',
+  LOGIN_COMPLETE: 'LOGIN_COMPLETE',
+
+  GET_ALL_DATA_REQUEST: 'GET_ALL_DATA_REQUEST',
+  GET_ALL_DATA_COMPLETE: 'GET_ALL_DATA_COMPLETE',
+
+  RELOAD_PAGE_REQUEST: 'RELOAD_PAGE_REQUEST',
+
+  LOGOUT_REQUEST: 'LOGOUT_REQUEST',
+  LOGOUT_COMPLETE: 'LOGOUT_COMPLETE',
+};
+export default AppFlowActions;
