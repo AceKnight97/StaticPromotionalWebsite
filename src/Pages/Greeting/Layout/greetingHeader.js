4a94f3f76823fe0c94e4a713616c32a5c7de4b04
triet <triettruong@itrvn.com>
2021-03-25 14:55:52 +0700

    add Greeting Header Menu

--- src/Pages/Greeting/Layout/greetingHeader.js
+++ src/Pages/Greeting/Layout/greetingHeader.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
+import { Menu } from 'antd';
+import { MailOutlined, AppstoreOutlined, SettingOutlined } from '@ant-design/icons';
 import { useHistory } from 'react-router-dom';
-import { } from 'antd';
+
+import { useMergeState } from '../../../Helpers/customHooks';
+
+const { SubMenu } = Menu;
 
 const GreetingHeader = (props) => {
   const history = useHistory();
+  const [state, setState] = useMergeState({
+    current: false,
+  });
+
+  const handleClick = (e) => {
+    console.log('click ', e);
+    setState({ current: e.key });
+  };
+
   const { className } = props;
+  const { current } = state;
   return (
     <div className='greeting-header'>
 
@@ -15,8 +30,27 @@ const GreetingHeader = (props) => {
       </div>
 
       <div className='greeting-header-right'>
-        <span>My CV</span>
-        <span>Buy Website</span>
+        <Menu onClick={handleClick} selectedKeys={[current]} mode='horizontal'>
+          <Menu.Item key='mail'>
+            Navigation One
+          </Menu.Item>
+          <Menu.Item key='app'>
+            Navigation Two
+          </Menu.Item>
+          <SubMenu key='SubMenu' title='Navigation Three - Submenu'>
+            <Menu.ItemGroup title='Item 1'>
+              <Menu.Item key='setting:1'>Option 1</Menu.Item>
+              <Menu.Item key='setting:2'>Option 2</Menu.Item>
+            </Menu.ItemGroup>
+            <Menu.ItemGroup title='Item 2'>
+              <Menu.Item key='setting:3'>Option 3</Menu.Item>
+              <Menu.Item key='setting:4'>Option 4</Menu.Item>
+            </Menu.ItemGroup>
+          </SubMenu>
+          <Menu.Item key='alipay'>
+            Navigation Four - Link
+          </Menu.Item>
+        </Menu>
       </div>
 
     </div>
