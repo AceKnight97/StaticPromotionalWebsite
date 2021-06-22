256b707ac2899f526acba1e6c17043f36ec2d36d
triet <triettruong@itrvn.com>
2021-06-22 19:28:59 +0700

    StartAt21June

---
+++ src/Pages/LightNovel/PerfectCultivator/temp.js
@@ -0,0 +1,30 @@
+const StartAt21June = `
+Prepare for the next review.
+
+I. Expecting Salary: 18 Millions VND.
+
+II. Reasons:
+  1. It's the standard price comparing to the IT field in HCM city.
+  2. I believe in my abilities and work experiences can handle the project by my own.
+  3. 
+
+III. Weaknesses: 
+  1. My current salary is 12 millions VND, it's a big distance.
+  2.
+  
+IV. Feelings:
+  1. I am the first to handle all the others's bugs (Back-end developer, Business Analysis, Designer).
+  2. I am the first to be responsible for any bug occurs.
+  3. 
+
+
+
+
+
+
+
+
+
+
+`;
+export default StartAt21June;
