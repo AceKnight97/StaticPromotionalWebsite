f8c46a23b064730f2e37b06ab6da57a7eeec4012
DESKTOP-419VCQ8\AceKnight <triettruong@itrvn.com>
2021-06-23 00:34:43 +0700

    done temp prepare

--- src/Pages/LightNovel/PerfectCultivator/temp.js
+++ src/Pages/LightNovel/PerfectCultivator/temp.js
@@ -1,25 +1,47 @@
+/* eslint-disable max-len */
 const StartAt21June = `
 Prepare for the next review.
 
 I. Expecting Salary: 18 Millions VND.
 
 II. Reasons:
-  1. It's the standard price comparing to the IT field in HCM city.
-  2. I believe in my abilities and work experiences can handle the project by my own.
-  3. 
-
-III. Weaknesses: 
-  1. My current salary is 12 millions VND, it's a big distance.
-  2.
+  1. It's the standard price compared to the 1.5 year-experience IT job in HCM city.
+  2. I believe my abilities and work experiences can handle the project on my own.
+  3. I've worked in this company for 2 years, I understand this company well enough to work smoothly.
   
-IV. Feelings:
-  1. I am the first to handle all the others's bugs (Back-end developer, Business Analysis, Designer).
-  2. I am the first to be responsible for any bug occurs.
-  3. 
+III. New responsibility I want to take:
+  1. The company is having more and more freshers or newbies. I can guide new people, sharing what I've learnt.
+    More work will be done.
+  2. If I have more responsibility, I'll work harder and faster.
 
+IV. What you may think: 
+  1. It's a big distance compared to my current salary (12 millions VND).
+  2. You may doubt my abilities and work experiences, in the other way, what I can handle.
+  3. The company is small, and can not pay much for the employee.
+  4. I am not worth that salary.
 
-
-
+V. My response to you:
+  1. I think it is not a problem. I think I've got so little from the beginning.
+    When the salary tells the abilities and work experiences, if you want to pay me low, that means you don't want me to work hard.
+    I deal the price as the market price, the standard price that the other companies can pay for their employees.
+  2. About my abilities and work experiences, what I can handle. 
+    I just want to say: everything you see I've done is just the minimum that I really have done. You have to admit that you can not see or know exactly what I've done all the time.
+  3. I don't think the company is still a small company. It's survived for many years, it has a big office and gonna to have another soon.
+    The company is getting bigger and bigger, telling me that it is still a small one is just an excuse. 
+  4. I'll prove I am, by this way or another.
+  
+VI. Feelings and thinkings:
+  1. I am the first one to handle all the others's bugs (Back-end developer, Business Analysis, Designer).
+  2. I am the first one to be responsible for any bug that occurs.
+  3. I think my salary is so low that even a newbie's salary is higher than mine.
+    Our company doesn't reveal people's salary, so no one can prove that I am right or wrong. 
+  4. The work has some unsuitable things: 
+    a. Quality Controller can log bugs for Front-end developers.
+  How about Back-end developer, Business Analysis and UI Designer? So unfair!
+    b. The time-line is fixed, it has no meaning except giving the stress for front-end developers.
+  Back-end can be late releasing the APIs, Business Analysis, Designer can change things that make the front-end developer have to work more and even overtime and no one cares about it.
+  People just care about how many bugs that the front-end developer has made and does the front-end developer release the product on time or not.
+    c. When I talked about the overtime dealing, leaders didn't respond the way I think they should.
 
 
 
